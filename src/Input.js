0ccee8217fe89d905e0139ba26825bd6375eda18
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-16 13:50:17 +0100

    add test for Input prop types

--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 /**
  * @function Input
  * @param  {Object} props - Props passed from parent component.
  * @returns {JSX.Element} - Jsx.
  */
-const Input = (props) => {
+const Input = ({ secretWord }) => {
   return (
     <div data-test="input-component">
       <div />
     </div>
   );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { checkElement } from "../test/testUtils";
+import { checkElement, checkProps } from "../test/testUtils";
 import Input from "./Input";
 
-const setup = (initialState = {}) => {
-  return shallow(<Input {...initialState} />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 describe("Input component", () => {
   test("Renders without crashing", () => {
     const wrapper = setup();
     checkElement(wrapper, "input-component");
+  });
+
+  test("has correct prop type for secretWord", () => {
+    const initialState = { secretWord: "party" };
+    checkProps(Input, initialState);
   });
 });
