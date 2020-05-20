bbbeba2099ab6bde1d47679d63437119c9bb46f2
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-20 13:49:40 +0100

    finished test for setInput state on change

--- src/Input.js
+++ src/Input.js
@@ -7,9 +7,35 @@ import PropTypes from "prop-types";
  * @returns {JSX.Element} - Jsx.
  */
 const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  const submitHandler = () => {
+    // TODO: update guessedWords
+    // TODO: check against secretWord and update success if needed.
+    // setCurrentGuess("");
+  };
   return (
     <div data-test="input-component">
-      <div />
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          value={currentGuess}
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter guess"
+          onChange={(e) => {
+            setCurrentGuess(e.target.value);
+          }}
+        />
+        <button
+          data-test="submit-button"
+          onClick={submitHandler}
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit Guess
+        </button>
+      </form>
     </div>
   );
 };
--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,10 @@ import React from "react";
 import { shallow } from "enzyme";
 import { checkElement, checkProps } from "../test/testUtils";
 import Input from "./Input";
-
+/**
+ * @param  {String} secretWord="party"
+ * @returns {ShallowWrapper}
+ */
 const setup = (secretWord = "party") => {
   return shallow(<Input secretWord={secretWord} />);
 };
@@ -16,5 +19,41 @@ describe("Input component", () => {
   test("has correct prop type for secretWord", () => {
     const initialState = { secretWord: "party" };
     checkProps(Input, initialState);
+  });
+});
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess;
+  let wrapper;
+  beforeEach(() => {
+    // Create a mock function for the setCurrentGuess function that updates the state.
+    mockSetCurrentGuess = jest.fn();
+
+    // Set up useState using React, it is a function that accepts one parameter and returns an array of two items: The initial State and a function to update it. So we make it a mock function that takes in an anonymous function and returns an Array with the same items, initial state and function.
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    // So we then setup a wrapper with our shallow wrapper method/
+    wrapper = setup();
+  });
+  test("state updates with value of input box", () => {
+    // we find the input box
+    const inputBox = wrapper.find("[data-test='input-box']");
+
+    // create a mock event to apply to the input.
+    const mockEvent = { target: { value: "party" } };
+
+    // simulate a change listener and apply the mock event target to the simulation.
+    inputBox.simulate("change", mockEvent);
+
+    // we expect the state update function to have been called with the right value of train.
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("party");
+  });
+
+  test("state is cleared on button click", () => {
+    const button = wrapper.find("[data-test='submit-button']");
+
+    button.simulate("click");
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });
