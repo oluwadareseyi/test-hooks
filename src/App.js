a1393bd68170b1b157912a316091d4584b16dacd
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-22 14:27:36 +0100

    code useReducer and useEffect for App component and pass tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
 import "./App.css";
+import hookActions from './actions/hookActions';
+
+/**
+ * @function reducer - Function to update state based on the action argumnent type.
+ * @param  {Object} state - The Initial State.
+ * @param  {Object} action - The action we dispatch to update the state. Contains 'type' and 'payload'
+ *                           properties. e.g. {type: "setSecretWord", payload: "party"}
+ * @returns {Object} - The updated state.
+ */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
 
 /**
  * @function App
@@ -7,6 +25,16 @@ import "./App.css";
  */
 
 const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: "setSecretWord", payload: secretWord });
+  };
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord)
+  }, []);
+
   return (
     <div data-test="app-component" className="App">
       <div />
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,41 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "./App";
 import { checkElement } from "../test/testUtils";
+import hookActions from "./actions/hookActions";
+
+// create a mock globally for all the tests.
+const mockGetSecretWord = jest.fn();
 /**
  * @function setup
  * @param  {Object} initialState={} - The initial state we would like to test with.
  * @returns {ShallowWrapper} - THe component wrapper.
  */
 const setup = (initialState = {}) => {
-  return shallow(<App {...initialState} />);
+  // clear the mock function before any call to make sure we don't have a mock already running.
+  mockGetSecretWord.mockClear();
+
+  // replace the getSecretWord action with the mock.
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount instead of shallow iuntil enzyme fixes the issue of useEffect not running on shallow     render.
+  // https://github.com/enzymejs/enzyme/issues/2086
+  return mount(<App {...initialState} />);
 };
 
 describe("app render", () => {
   test("renders without error", () => {
     const wrapper = setup();
     checkElement(wrapper, "app-component");
+  });
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    // run the setup function, we don't even need to save an instance. We just run to mount the component.
+    setup();
+
+    // expect mockSecretWord to be called.
+    expect(mockGetSecretWord).toHaveBeenCalled();
   });
 });
